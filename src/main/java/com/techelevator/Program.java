088100afba2b26d46c1c43d6e6c0240b46c2e5f8
Robert Lutsock <rlutsock@gmail.com>
2022-10-07 11:57:41 -0400

    beginning vending process

--- src/main/java/com/techelevator/Program.java
+++ src/main/java/com/techelevator/Program.java
@@ -6,13 +6,14 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Program {
 
-	public static void main(String[] args) throws FileNotFoundException
-
-	{
+	public static void main(String[] args) throws FileNotFoundException {
 		Map<String, Product> productMap = new HashMap<>();
+		User user = new User();
+
 		String invPath = "C:\\Users\\Student\\workspace\\java-minicapstonemodule1-team4\\vendingmachine.csv";
 		File invFile = new File(invPath);
 		try (Scanner invScanner = new Scanner(invFile)) {
@@ -25,12 +26,54 @@ public class Program {
 				String productType = productParts[3];
 				productMap.put(productLocation, new Product(productName, productPrice, productType));
 
+			}
+		}
+
+
+		System.out.println("*************************************************************");
+		System.out.println("*Very-Convenient-Vending-Service-Tron-O-Matic-1006-PLUS-9001*");
+		System.out.println("*************************************************************");
+
+		// menu
+
+		Scanner userRead = new Scanner(System.in);
+
+
+		System.out.println("(1) Display Vending Machine Items"); // print inventory and return to this menu
+		System.out.println("(2) Purchase");                     //
+		System.out.println("(3) Exit");
+		String userChoice = userRead.nextLine();        // expecting 1 - 3
+
+		while (userChoice != "3"){
+			if (userChoice.equals("1")) {
+				//output inventory screen and return to menu
+				Set<String> keys = productMap.keySet();
+				for (String key : keys){
+					System.out.println(key +" "+ productMap.get(key).getName() + " " + productMap.get(key).getPrice() + " (" + productMap.get(key).getStock()+ " in stock)");
+
+				}
+		}
+			else if(userChoice.equals("2")) {
+				System.out.println("(1) Feed Money");
+				System.out.println("(2) Select Product");
+				System.out.println("(3) Finish Transaction");
+				String purchaseChoice = userRead.nextLine();
+				while(purchaseChoice ){
+
+
+				}
+
 
 
 			}
-		}
-	}
 
+
+			//user input #
+
+
+		}
+
+	}
 }
 
 
--- src/main/java/com/techelevator/Transaction.java
+++ src/main/java/com/techelevator/Transaction.java
@@ -1,12 +1,62 @@
 package com.techelevator;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Transaction {
+// output/format for sales log =  " currentDate + currentTime + AM/PM + transactionType ( feed / name of item+location / give change)
+    // amount deposited/spent/change + amount of new balance post-prior-change.
+
+
 
     // Properties
+    private SimpleDateFormat dateTime = new SimpleDateFormat("MM/dd/yyyy hh.mm.ss aa");
+    private String transactionType;
+    private double currentBalance;
+    private Product dispensedItem;
+    private String currentTime = dateTime.format(new Date()).toString();
+    double fedMoney;
+
+
+
 
     // Constructors
+        public Transaction (Product dispensedItem){
+            this.currentTime = currentTime;
+            this.dispensedItem = dispensedItem;
+            this.currentBalance = currentBalance;
+        }
+
 
     // G&S
 
     // Methods
+
+    //
+
+
+
+    public void feedMoney (double fedMoney){
+                 //receives a double and adds it to the currentBalance.
+                // returns current balance with fed money added.
+                // write a line to account for the change in stock.
+
+        currentBalance = currentBalance + fedMoney;
+
+    }
+
+    public void dispenseTransaction (Product product){
+
+    }
+
+    /* todo - write three methods for individual transactions
+    1- feed money transaction method : write to file saying : Date, Time, AM/PM, "FEED MONEY:", Initial balance, post change balance.
+    2- dispense product method:  Date, Time, AM/PM, "itemName + location", Initial balance, post change balance.
+    3- give change method Date, Time, AM/PM, "GIVE CHANGE", Initial balance, post change balance ( WHICH SHOULD BE ZERO AFTER )
+
+    */
+
 }
